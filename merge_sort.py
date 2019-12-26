5dbbd4fea26a0f8ead1f53e258d2330d7d838790
root <root@winterfell.novalocal>
2019-12-26 07:09:55 +0000

    Merge sort

---
+++ merge_sort.py
@@ -0,0 +1,25 @@
+def merge_sort(nums):
+    print(nums)
+    len_nums = len(nums)
+    if len_nums == 1:
+        return nums
+
+    mid = len_nums // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    sorted_nums = list()
+    left_index,right_index = 0,0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            sorted_nums.append(left[left_index])
+            left_index += 1
+        else:
+            sorted_nums.append(right[right_index])
+            right_index += 1
+    if left_index < (len(left)):
+        sorted_nums.extend(left[left_index:])
+    else:
+        sorted_nums.extend(right[right_index:])
+ 
+    return sorted_nums
