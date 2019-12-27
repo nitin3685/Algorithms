fae8853c9853b6a892bec5268061e3e04b699af5
root <root@winterfell.novalocal>
2019-12-27 06:25:26 +0000

    Slicing doesnt work in all cases.

--- karatsuba_mul.py
+++ karatsuba_mul.py
@@ -1,22 +1,27 @@
 #https://en.m.wikipedia.org/wiki/Karatsuba_algorithm#Pseudocode
 #https://stackoverflow.com/questions/42324419/karatsuba-multiplication-implementation
+def karatsuba(x,y):
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x*y
+    else:
+        m = max(len(str(x)),len(str(y)))
+        m2 = m // 2
 
-def karatsuba(num1,num2):
-    if (num1 < 10) or (num2 < 10):
-        return num1 * num2
+        a = x // 10**(m2)
+        b = x % 10**(m2)
+        c = y // 10**(m2)
+        d = y % 10**(m2)
+        #print(m2,x,y,a,b,c,d)
+        z0 = karatsuba(b,d)
+        z1 = karatsuba((a+b),(c+d))
+        z2 = karatsuba(a,c)
 
-    str_num1 = str(num1)
-    str_num2 = str(num2)
-    m = min(len(str_num1),len(str_num2))
-    mid = m//2
+        return (z2 * 10**(2*m2)) + ((z1 - z2 - z0) * 10**(m2)) + (z0)
 
-    a = int(str_num1[:mid])
-    b = int(str_num1[mid:])
-    c = int(str_num2[:mid])
-    d = int(str_num2[mid:])
+'''
+print(karatsuba(65456,34453))
+print(65456*34453)
 
-    z0 = karatsuba(b,d)
-    z1 = karatsuba((a+b),(c+d))
-    z2 = karatsuba(a,c)
-
-    return (z2*(10**(2*mid))) + ((z1-z2-z0) * 10**mid) + z0
+print(karatsuba(3141592653589793238462643383279502884197169399375105820974944592,2718281828459045235360287471352662497757247093699959574966967627))
+print(3141592653589793238462643383279502884197169399375105820974944592*2718281828459045235360287471352662497757247093699959574966967627)
+'''
